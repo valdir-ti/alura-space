1edeb09eeab47444666da26101c002181177702e
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-08 16:07:38 -0300

    feat: add publicada field and make it editable on list

--- galeria/admin.py
+++ galeria/admin.py
@@ -8,10 +8,12 @@ from galeria.models import Fotografia
 '''list filter: serve para definir um campo para filtrar por uma lista'''
 '''list_per_page determina uma paginação e quantos itens devem aparecer por vez'''
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda', 'foto')
+    list_display = ('id', 'nome', 'legenda', 'foto', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
     list_filter = ('categoria', )
+    list_editable = ('publicada', )
     list_per_page = (10)
+
 
 admin.site.register(Fotografia, ListandoFotografias)
