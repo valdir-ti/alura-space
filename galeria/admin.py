923a0463c246bef2bb8e0af5ea8f29c5fc0167f1
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-08 15:34:25 -0300

    feat: crud no admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+'''Configurações da listagem de fotografias'''
+'''list_display serve para mostrar os campos na listagem'''
+'''list_display_links serve para indicar quais campos podemos abrir para edição'''
+'''search_fields serve para definir quais campos podemos utilizar para realizar buscas'''
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'foto')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+admin.site.register(Fotografia, ListandoFotografias)
