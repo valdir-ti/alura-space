073dfcd0f258d6b9c7f11cbbc3c5badf7c0ef967
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-08 10:38:25 -0300

    feat: envio de dados através da function

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {
+            'nome': 'Nebulosa de Carina',
+            'legenda': 'webbtelescope / NASA / James Webb'
+        },
+        2: {
+            'nome': 'Galaxia NGC 1079',
+            'legenda': 'webbtelescope / NASA / Hubble'
+        }
+    }
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
