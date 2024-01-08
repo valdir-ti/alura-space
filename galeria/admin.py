57bbb71c4873a8aa7c7e017c08c983cdac1ac078
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-08 15:54:04 -0300

    feat: config admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,9 +5,13 @@ from galeria.models import Fotografia
 '''list_display serve para mostrar os campos na listagem'''
 '''list_display_links serve para indicar quais campos podemos abrir para edição'''
 '''search_fields serve para definir quais campos podemos utilizar para realizar buscas'''
+'''list filter: serve para definir um campo para filtrar por uma lista'''
+'''list_per_page determina uma paginação e quantos itens devem aparecer por vez'''
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda', 'foto')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
+    list_filter = ('categoria', )
+    list_per_page = (10)
 
 admin.site.register(Fotografia, ListandoFotografias)
